b831337262ee3ac74aa59061dcb391898df654c7
jjanczynska <joaxjan@gmail.com>
2023-06-22 17:18:45 +0000

    Customize the bootstrap and start adding content

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 # creating an instance of Flask class and soring it in name variable called name- a build in python variable(templates and static files)
 app = Flask(__name__)
 
-# app root decorator(py notation) a way of wrapping functions. if we try to browse through the rout directory indicated by "/" - the functions underneath is triggerd
+# app root director(py notation) a way of wrapping functions. if we try to browse through the rout directory indicated by "/" - the functions underneath is triggerd
 @app.route("/")
 def index():
     return render_template("index.html")
